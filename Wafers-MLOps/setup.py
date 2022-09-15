dc279515fcf1ad389fcbbd3cdb60bd88707f9140
Pushkar Deshpande <47081733+pushkya@users.noreply.github.com>
2022-09-15 16:20:24 +0530

    Add files via upload

---
+++ Wafers-MLOps/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Classification project on Wafer dataset and applying MLOps to get hands on experience',
+    author='Pushkar Deshpande',
+    license='MIT',
+)
